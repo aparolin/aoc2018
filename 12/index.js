50a3ebbb947de9890ca0eb97d03b4db74ca686da
Alessandro Parolin <alessandro.parolin@gmail.com>
2018-12-17 21:04:34 -0200

    day 12 part 2

--- 12/index.js
+++ 12/index.js
@@ -38,33 +38,23 @@ function shouldBeAlive(state, rules, index){
 }
 
 function run(initialState, rules, generations) {
-  const seenStates = new Map();
-  const nextStates = [];
   let state = initialState;
   
-  //include some pots at the edges
-  const additionaPots = 15000;
-  let pots = new Array(additionaPots).fill('.').join('');
-  state = pots + state + pots;
-  leftMostIndex = -additionaPots;
-  
-  nextStates.push(state);
+  let leftMostIndex = 0;
 
   // console.log(`0: ${state}`);
   for (let i = 0; i < generations; i++){
-    //if we reach this point, we will not be in a loop
-    //no need to keep iterating over the states
-    if (seenStates.has(state)){
-      const amountStates = nextStates.length;
-      const nextStatePosition = seenStates.get(state);
-      const delta = amountStates - nextStatePosition;
-
-      const missingIterations = generations - i;
-      const finalStatePosition = nextStatePosition + (missingIterations % delta) - 1;
-      state = nextStates[finalStatePosition];
-      break;
+    //add aditional plant to the beginning
+    if (state[0] === '#' || state[1] === '#'){
+      state = '.' + state;
+      leftMostIndex--;
     }
-    
+
+    //add aditional plants at the end
+    if (state[state.length-2] === '#' || state[state.length-1] === '#'){
+      state = state + '.'
+    }
+
     let newState = '';
     for (let j = 0; j < state.length; j++){
       if (shouldBeAlive(state, rules, j)){
@@ -73,10 +63,6 @@ function run(initialState, rules, generations) {
         newState += '.';
       }
     }
-
-    //add the position of the seen state in the array
-    nextStates.push(newState);
-    seenStates.set(state, nextStates.length-1);
 
     state = newState;
     // console.log(`${i+1}: ${state}`);
@@ -91,6 +77,26 @@ function run(initialState, rules, generations) {
   return sumPotsContainingPlants
 }
 
+function part2(){
+  /*
+  The next algorithm was deduced empirically. By printing around 200 generations of plants, the pattern below can be seen prepended by lots of empty pots.
+  In generation=185, the first plant is prepended by 145 empty spots and the leftMostIndex=-3. In each iteration, the pattern move one to the right, but the leftMostIndex stay the same.
+  By following the above logic, the end result can be determined as follows.
+  */
+  const pattern = '##..#......##..#......##..#......##..#......##..#....##..#.......##..#......##..#....##..#.....##..#......##..#......##..#....##..#....##..#';
+  //185-145 = 40
+  const index = 50000000000-40;
+  
+  let sum = 0;
+  for (let i = 0; i < pattern.length; i++){
+    if (pattern[i] === '#'){
+      //-3 because of the leftMostIndex
+      sum += index + i -3
+    }
+  }
+  return sum;
+}
+
 const start = new Date();
 
 const fs = require('fs');
@@ -98,8 +104,7 @@ const input = fs.readFileSync('input.txt').toString();
 const parsedInput = parse(input);
 
 console.log(`Part 1: ${run(parsedInput.initialState, parsedInput.rules, 20)}`);
-console.log(`Part 2: ${run(parsedInput.initialState, parsedInput.rules, 50000000000)}`);
+console.log(`Part 2: ${part2()}`);
 
 const elapsed = new Date() - start;
 console.log(`Total time: ${elapsed}ms`);
-
