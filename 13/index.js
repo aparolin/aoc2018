4c86e6d3602ef6360d013ce8e3b43a403e1eb3c6
Alessandro Parolin <alessandro.parolin@gmail.com>
2018-12-18 07:41:29 -0200

    day 13 - issue on curves

---
+++ 13/index.js
@@ -0,0 +1,212 @@
+const directions = {
+  up: [-1, 0],
+  right: [0, 1],
+  down: [1, 0],
+  left: [0, -1]
+}
+
+function isCart(char){
+  return char === '^' || char === '>' || char === 'v' || char === '<';
+}
+
+function cartDirection(char){
+  switch (char){
+    case '^':
+      return directions.up;
+    case '>':
+      return directions.right;
+    case 'v':
+      return directions.down;
+    case '<':
+      return directions.left;
+    default:
+      throw new Error(`Can't determine direction of unexpected char ${char}`);
+  }
+}
+
+function parseFile(fileName){
+  const fs = require('fs');
+  const input = fs.readFileSync(fileName).toString();
+  const lines = input.split('\r\n');
+  
+  let grid = [];
+  const carts = [];
+  for (let row = 0; row < lines.length; row++) {
+    grid[row] = new Array(lines[row].length);
+  
+    for (let col = 0; col < lines[row].length; col++) {
+      let char = lines[row][col];
+      grid[row][col] = char;
+  
+      if (isCart(char)){
+        const newCart = {
+          position: [row, col],
+          direction: cartDirection(char),
+          turns: 0,
+          turnLeft: function(){
+            switch (this.direction.join(',')) {
+              case directions.down.join(','):
+                this.direction = directions.right;
+                break;
+              case directions.right.join(','):
+                this.direction = directions.up;
+                break;
+              case directions.up.join(','):
+                this.direction = directions.left;
+                break;
+              case directions.left.join(','):
+                this.direction = directions.down;
+                break;
+            }
+          },
+          turnRight: function(){
+            switch (this.direction.join(',')) {
+              case directions.down.join(','):
+                this.direction = directions.left;
+                break;
+              case directions.left.join(','):
+                this.direction = directions.up;
+                break;
+              case directions.up.join(','):
+                this.direction = directions.right;
+                break;
+              case directions.right.join(','):
+                this.direction = directions.down;
+                break;
+            }
+          },
+          intersection: function(){
+            this.turns++;
+            switch (this.turns){
+              case 1:
+                return this.turnLeft()
+              case 3:
+                this.turns = 0;
+                return this.turnRight();
+            }
+          }
+        };
+        carts.push(newCart);
+
+        //do not consider the cart on the grid
+        if (newCart.direction === directions.up || newCart.direction === directions.down){
+          grid[row][col] = '|';
+        } else {
+          grid[row][col] = '-';
+        }
+      }
+    }
+  }
+  
+  return {
+    carts,
+    grid
+  }  
+}
+
+function update(grid, cart){
+  // const prevPos = cart.position;
+  const nextPos = [cart.position[0] + cart.direction[0], cart.position[1] + cart.direction[1]];
+  const nextPath = grid[nextPos[0]][nextPos[1]];
+  
+  cart.position = nextPos;
+
+  let collisionCoords = null;
+  switch (nextPath) {
+    case '\\':
+      if (cart.direction[1] === 1){
+        cart.direction = directions.down;
+      } else if (cart.direction[1] === -1) {
+        cart.direction = directions.up;
+      } else if (cart.direction[0] === -1) {
+        cart.direction = directions.left;
+      }
+      break;
+    case '/':
+      if (cart.direction[1] === 1){
+        cart.direction = directions.up;
+      } else {
+        cart.direction = directions.down;
+      }
+      break;
+    case '+':
+      cart.intersection();
+      break;
+  }
+}
+
+function print(grid, carts){
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[row].length; col++) {
+      let printCart = false;
+      for (let k = 0; k < carts.length; k++){
+        if (carts[k].position[0] === row && carts[k].position[1] === col){
+          if (carts[k].direction[0] === -1 && carts[k].direction[1] === 0){
+            process.stdout.write('^');
+            printCart = true;
+          }
+          if (carts[k].direction[0] === 0 && carts[k].direction[1] === 1){
+            process.stdout.write('>');
+            printCart = true;
+          }
+          if (carts[k].direction[0] === 1 && carts[k].direction[1] === 0){
+            process.stdout.write('v');
+            printCart = true;
+          }
+          if (carts[k].direction[0] === 0 && carts[k].direction[1] === -1){
+            process.stdout.write('<');
+            printCart = true;
+          }
+        }
+      }
+
+      if (!printCart){
+        process.stdout.write(grid[row][col]);
+      }
+    }
+    process.stdout.write('\n');
+  }
+}
+
+function part1(){
+  const {carts, grid} = parseFile('sample_input.txt');
+
+  while (true){
+    print(grid, carts)
+
+    for (let i = 0; i < carts.length; i++){
+      update(grid, carts[i]);
+      
+      //check collisions
+      for (let j = 0; j < carts.length; j++){
+        if (i === j){
+          continue;
+        }
+
+        if (carts[i].position[0] === carts[j].position[0] && 
+          carts[i].position[1] === carts[j].position[1]){
+            //invert the coords so that we get X,Y format
+            return `${carts[i].position[1]},${carts[i].position[0]}`;
+          }
+      }
+    }
+
+    //sort carts
+    carts.sort((c1,c2) => {
+      if (c1.position[0] < c2.position[0]){
+        return -1;
+      }
+      if (c1.position[0] === c2.position[0] && c1.position[1] < c2.position[1]){
+        return -1;
+      }
+
+      return 1;
+    });
+  }
+}
+
+const start = new Date();
+console.log(`Part 1: ${part1()}`);
+
+const elapsed = new Date() - start;
+console.log(`Total time: ${elapsed}ms`);
